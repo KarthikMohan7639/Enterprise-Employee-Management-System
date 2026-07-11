f333af9469115b04bd0168fce5c36e043473ad0e
KarthikMohan7639 <karthikmohan7639@gmail.com>
2026-07-11 15:53:56 +0530

    delete and add

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/reset.css';
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
