c8ad889453c38f6fb35a30f27ffbff84a77dde3c
KarthikMohan7639 <karthikmohan7639@gmail.com>
2026-07-11 22:03:21 +0530

    feat(exception): add ResourceNotFoundException

--- backend/employee-service/src/main/java/com/eems/employee/response/ResourceNotFoundException.java
+++ backend/employee-service/src/main/java/com/eems/employee/response/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package com.eems.employee.response;
 
-public class ResourceNotFoundException {
-    
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
